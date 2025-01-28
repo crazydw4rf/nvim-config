141766f56623286f446e0f98fd1ab8f56ccd866d
Binar Nugroho <binarnugroho775@gmail.com>
2025-01-28 19:41:57 +0700

    feat(plugin): add bash language server

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Bash languange server
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "sh", "zshrc", "bashrc", "bash" },
+  callback = function()
+    vim.lsp.start({
+      name = "bash-language-server",
+      cmd = { "bash-language-server", "start" },
+    })
+  end,
+})
