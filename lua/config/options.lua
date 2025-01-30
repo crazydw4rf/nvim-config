7762f77759bdeb09625be53f192c52a747470687
Binar Nugroho <binarnugroho775@gmail.com>
2025-01-30 15:44:53 +0700

    disable relative number

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
 
--- LSP Server to use for PHP.
--- Set to "intelephense" to use intelephense instead of phpactor.
-vim.g.lazyvim_php_lsp = "intelephense"
+opt.relativenumber = false
 
 vim.filetype.add({
   pattern = {
