657977dd4b27e0bc9274340105563031120f21d3
Binar Nugroho <binarnugroho775@gmail.com>
2025-03-09 00:01:21 +0700

    update key bindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,8 +9,9 @@ map("n", "|", "v")
 map("i", "\\\\", "<ESC>")
 map("n", ";", ":")
 
-map({ "n", "v" }, "<leader>e", "<CMD>Neotree focus<CR>", { desc = "Focus to Neotree" })
 map({ "n", "v" }, "<F2>", "<CMD>lua vim.lsp.buf.rename()<CR>", { desc = "Rename variable" })
-map({ "n" }, "<C-Tab>", "<CMD>BufferLineCycleNext<CR>", { desc = "Next buffer" })
-map("i", "<C-BS>", "<ESC>diwi", { desc = "Delete whole word" })
-map("n", "<leader>x", " bd", { desc = "Close buffer" })
+
+map("n", "_", "<CMD>BufferLineCyclePrev<CR>", { desc = "Change to previous buffer" })
+map("n", "+", "<CMD>BufferLineCycleNext<CR>", { desc = "Change next buffer" })
+
+map({ "n", "v" }, "<leader>dd", '"_dd', { desc = "Delete line without yanking" })
