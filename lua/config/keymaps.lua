022582de1d8385d6bef4ebe790b98075c460cb65
Binar Nugroho <binarnugroho775@gmail.com>
2025-03-31 10:12:02 +0700

    Update lazy-lock.json and keymaps.lua
    - Updated plugin commits in lazy-lock.json
    - Added new key mappings in keymaps.lua:
      - "jj" to escape insert mode
      - <CR> to insert a new line below in normal mode
      - <S-CR> to insert a new line above in

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,6 +7,7 @@ local map = vim.keymap.set
 map("n", "\\", "i")
 map("n", "|", "v")
 map("i", "\\\\", "<ESC>")
+map("i", "jj", "<ESC>")
 map("n", ";", ":")
 
 map({ "n", "v" }, "<F2>", "<CMD>lua vim.lsp.buf.rename()<CR>", { desc = "Rename variable" })
@@ -21,3 +22,6 @@ map({ "n", "v" }, "<Up>", '<CMD>echo "↑ lol. no."<CR>', { noremap = true })
 map({ "n", "v" }, "<Left>", '<CMD>echo "Left? Really? Did your HJKL keys break?"<CR>', { noremap = true })
 map({ "n", "v" }, "<Down>", '<CMD>echo "Down? Are you lost? Try ’j‘ like a pro."<CR>', { noremap = true })
 map({ "n", "v" }, "<Right>", '<CMD>echo "Right... because ’l‘ was just too efficient?"<CR>', { noremap = true })
+
+map("n", "<CR>", "m`o<Esc>")
+map("n", "<S-CR>", "m`O<Esc>")
